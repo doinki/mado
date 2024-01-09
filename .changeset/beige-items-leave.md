589c984a85221938162c43d2426fe56168f3422c
doinki <doinki94@gmail.com>
2024-01-10 03:42:47 +0900

    feat(invariant): 🎉

---
+++ .changeset/beige-items-leave.md
@@ -0,0 +1,5 @@
+---
+'@mado/invariant': patch
+---
+
+feat(invariant): 🎉
---
+++ packages/mado-invariant/test/invariant.test.ts
@@ -0,0 +1,94 @@
+import { describe, expect, test } from 'vitest';
+
+import { invariant, InvariantError, invariantResponse } from '../src';
+
+describe('InvariantError', () => {
+  test('should be an instance of Error', () => {
+    expect(new InvariantError('')).toBeInstanceOf(Error);
+  });
+
+  test('should be an instance of InvariantError', () => {
+    expect(new InvariantError('')).toBeInstanceOf(InvariantError);
+  });
+
+  test('should have a name', () => {
+    expect(new InvariantError('')).toHaveProperty('name', 'InvariantError');
+  });
+
+  test('should have a message', () => {
+    expect(new InvariantError('')).toHaveProperty('message', '');
+  });
+
+  test('should have a stack trace', () => {
+    expect(new InvariantError('')).toHaveProperty('stack');
+  });
+});
+
+describe('invariant', () => {
+  test('should not throw if condition is truthy', () => {
+    expect(() => invariant(true, '')).not.toThrow();
+  });
+
+  test('should throw if condition is falsy', () => {
+    expect(() => invariant(false, '')).toThrow(InvariantError);
+  });
+
+  test('should throw with the given message', () => {
+    expect(() => invariant(false, 'message')).toThrow('message');
+  });
+
+  test('should throw with the given message function', () => {
+    expect(() => invariant(false, () => 'message')).toThrow('message');
+  });
+});
+
+describe('invariantResponse', () => {
+  test('should not throw if condition is truthy', () => {
+    expect(() => invariantResponse(true, '')).not.toThrow();
+  });
+
+  test('should throw if condition is falsy', () => {
+    expect(() => invariantResponse(false, '')).toThrow(Response);
+  });
+
+  test('should throw with the given message', async () => {
+    try {
+      invariantResponse(false, 'message');
+
+      expect.fail('should not be called');
+    } catch (error) {
+      invariant(error instanceof Response, 'should be a Response');
+
+      expect(error.ok).toBe(false);
+      expect(error.status).toBe(400);
+      expect(await error.text()).toBe('message');
+    }
+  });
+
+  test('should throw with the given message function', async () => {
+    try {
+      invariantResponse(false, () => 'message');
+
+      expect.fail('should not be called');
+    } catch (error) {
+      invariant(error instanceof Response, 'should be a Response');
+
+      expect(error.ok).toBe(false);
+      expect(error.status).toBe(400);
+      expect(await error.text()).toBe('message');
+    }
+  });
+
+  test('should throw with the given responseInit', () => {
+    try {
+      invariantResponse(false, '', { status: 500 });
+
+      expect.fail('should not be called');
+    } catch (error) {
+      invariant(error instanceof Response, 'should be a Response');
+
+      expect(error.ok).toBe(false);
+      expect(error.status).toBe(500);
+    }
+  });
+});
