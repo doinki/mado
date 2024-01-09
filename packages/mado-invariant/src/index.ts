c4cd84d32386e1e6f2f5d6eedc52fcc5f7ddb0ea
doinki <90969158+doinki@users.noreply.github.com>
2024-01-10 03:43:28 +0900

    feat(invariant): 🎉 (#3)

---
+++ packages/mado-invariant/src/index.ts
@@ -0,0 +1,41 @@
+export class InvariantError extends Error {
+  constructor(message: string) {
+    super(message);
+
+    this.name = 'InvariantError';
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, InvariantError);
+    }
+  }
+}
+
+/**
+ * @throws {InvariantError}
+ */
+export function invariant(
+  condition: unknown,
+  message: string | (() => string),
+): asserts condition {
+  if (!condition) {
+    throw new InvariantError(
+      typeof message === 'function' ? message() : message,
+    );
+  }
+}
+
+/**
+ * @throws {Response}
+ */
+export function invariantResponse(
+  condition: unknown,
+  message: string | (() => string),
+  responseInit?: ResponseInit,
+): asserts condition {
+  if (!condition) {
+    throw new Response(typeof message === 'function' ? message() : message, {
+      status: 400,
+      ...responseInit,
+    });
+  }
+}
