2120e502168f5e5282e224b075e1f34e049b715c
doinki <doinki94@gmail.com>
2024-01-26 12:06:23 +0900

    feat(utils): Add isObjectLike and isPlainObject functions

---
+++ packages/mado-utils/test/isObjectLike.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, test } from 'vitest';
+
+import { isObjectLike } from '../src';
+
+describe('isObjectLike', () => {
+  test.each([{}, { a: 1 }, Object.create(null), new Date(), []])(
+    'should return true',
+    (a) => {
+      expect(isObjectLike(a)).toBe(true);
+    },
+  );
+
+  test.each([null, undefined, false, 0, NaN, '', () => {}, Symbol('')])(
+    'should return false',
+    (a) => {
+      expect(isObjectLike(a)).toBe(false);
+    },
+  );
+});
