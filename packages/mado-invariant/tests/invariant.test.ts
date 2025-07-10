47a75214e9920b488b7cedee65d0ced1e67a7fb7
doinki <doinki94@gmail.com>
2025-07-11 00:49:05 +0900

    refactor(invariant): ♻️

--- packages/mado-invariant/tests/invariant.test.ts
+++ packages/mado-invariant/tests/invariant.test.ts
@@ -1,35 +1,35 @@
-import { describe, expect, test } from 'vitest';
+import { describe, expect, it } from 'vitest';
 
 import { invariant, InvariantError, invariantResponse, isInvariantError } from '../src';
 
 describe('InvariantError', () => {
-  test('should be an instance of Error', () => {
+  it('should be an instance of Error', () => {
     expect(new InvariantError('')).toBeInstanceOf(Error);
   });
 
-  test('should be an instance of InvariantError', () => {
+  it('should be an instance of InvariantError', () => {
     expect(new InvariantError('')).toBeInstanceOf(InvariantError);
   });
 
-  test('should have a name', () => {
+  it('should have a name', () => {
     expect(new InvariantError('')).toHaveProperty('name', 'InvariantError');
   });
 
-  test('should have a message', () => {
+  it('should have a message', () => {
     expect(new InvariantError('')).toHaveProperty('message', '');
   });
 
-  test('should have a stack trace', () => {
+  it('should have a stack trace', () => {
     expect(new InvariantError('')).toHaveProperty('stack');
   });
 });
 
 describe('isInvariantError', () => {
-  test('should return true if error is an InvariantError', () => {
+  it('should return true if error is an InvariantError', () => {
     expect(isInvariantError(new InvariantError(''))).toBe(true);
   });
 
-  test.each([new Error(), 0, false, '', Symbol(''), null, undefined, {}, () => {}])(
+  it.each([new Error(), 0, false, '', Symbol(''), null, undefined, {}, () => {}])(
     'should return false if error is not an InvariantError',
     (a) => {
       expect(isInvariantError(a)).toBe(false);
@@ -38,25 +38,25 @@ describe('isInvariantError', () => {
 });
 
 describe('invariant', () => {
-  test('should not throw if condition is truthy', () => {
+  it('should not throw if condition is truthy', () => {
     expect(() => {
       invariant(true, '');
     }).not.toThrow();
   });
 
-  test('should throw if condition is falsy', () => {
+  it('should throw if condition is falsy', () => {
     expect(() => {
       invariant(false, '');
     }).toThrow(InvariantError);
   });
 
-  test('should throw with the given message', () => {
+  it('should throw with the given message', () => {
     expect(() => {
       invariant(false, 'message');
     }).toThrow('message');
   });
 
-  test('should throw with the given message function', () => {
+  it('should throw with the given message function', () => {
     expect(() => {
       invariant(false, () => 'message');
     }).toThrow('message');
@@ -64,19 +64,19 @@ describe('invariant', () => {
 });
 
 describe('invariantResponse', () => {
-  test('should not throw if condition is truthy', () => {
+  it('should not throw if condition is truthy', () => {
     expect(() => {
       invariantResponse(true, '');
     }).not.toThrow();
   });
 
-  test('should throw if condition is falsy', () => {
+  it('should throw if condition is falsy', () => {
     expect(() => {
       invariantResponse(false, '');
     }).toThrow(Response);
   });
 
-  test('should throw with the given message', async () => {
+  it('should throw with the given message', async () => {
     try {
       invariantResponse(false, 'message');
 
@@ -90,7 +90,7 @@ describe('invariantResponse', () => {
     }
   });
 
-  test('should throw with the given message function', async () => {
+  it('should throw with the given message function', async () => {
     try {
       invariantResponse(false, () => 'message');
 
@@ -104,7 +104,7 @@ describe('invariantResponse', () => {
     }
   });
 
-  test('should throw with the given responseInit', () => {
+  it('should throw with the given responseInit', () => {
     try {
       invariantResponse(false, '', { status: 500 });
 
